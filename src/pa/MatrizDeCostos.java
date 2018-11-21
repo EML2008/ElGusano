436fa78eff2edf19f9d45f7c5ba71db99d2640b2
elancuba <lancuba.emiliano@gmail.com>
2018-11-21 20:12:33 -0300

    MatrizDeCostos

--- src/pa/MatrizDeCostos.java
+++ src/pa/MatrizDeCostos.java
@@ -5,29 +5,32 @@ public class MatrizDeCostos {
 	private int cantidadDeNodos;
 
 	public MatrizDeCostos(int cantidadDeNodos) {
+		this.cantidadDeNodos = cantidadDeNodos;
 		this.matriz = new int[cantidadDeNodos][];
 		for (int i = 0; i < cantidadDeNodos; i++) {
 			matriz[i] = new int[i];
 		}
 	}
-	
-	public void set(int fila,int columna,int costo)
-	{
-		if(columna == fila)
-		{
+
+	public void set(int fila, int columna, int costo) {
+		if (columna == fila) {
 			this.matriz[columna][columna] = 0;
 			return;
 		}
-		if(columna>fila)
-		{
+		if (columna > fila) {
 			this.matriz[columna][fila] = costo;
 			return;
 		}
 		this.matriz[fila][columna] = costo;
 	}
-	
-	public int get(int n1,int n2)
-	{
-		
+
+	public int get(int fila, int columna) {
+		if (columna > fila)
+			return this.matriz[columna][fila];
+		return this.matriz[fila][columna];
+	}
+
+	public int getCantidadDeNodos() {
+		return cantidadDeNodos;
 	}
 }
