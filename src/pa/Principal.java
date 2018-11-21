4a73faa9dd43cd1138573257e1d43c4aedf1fce3
joniborda <jonathanmatiasborda@gmail.com>
2018-11-21 20:45:19 -0300

    principal

---
+++ src/pa/Principal.java
@@ -0,0 +1,10 @@
+package pa;
+
+import java.io.FileNotFoundException;
+
+public class Principal {
+
+	public static void main(String args[]) throws FileNotFoundException {
+		Gusano gusano = ArchivoElGusano.leerArchivo("archivos/consigna.in");
+	}
+}
